2e658a8c2d86cdcb0a9828bea27f37e04e1e8633
Diego Islas Ocampo <hyuchia@gmail.com>
2017-01-06 00:48:35 -0600

    Add missing meta data

--- lib/templates/main.php
+++ lib/templates/main.php
@@ -12,6 +12,10 @@
         public $_title = "Skrifa";
 		public $_description = "A simple word processor built with web technologies.";
 		public $_keywords = "editor, word, skrifa, processor, notes, notebook, scholl, office, encrypted, pgp";
+		public $_shareimage = "share.png";
+		public $_author = "Hyuchia";
+		public $_twitter = "@HyuchiaDiego";
+		public $_google = "+HyuchiaDiego";
 
 		public $year;
 
